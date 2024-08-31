ec2851c0d08ff416c728a7ee58735d755a8a71c3
Cristiano Costa <107226337+CCostaDev@users.noreply.github.com>
2024-08-31 19:38:45 +0100

    changes to server handling

--- server.js
+++ server.js
@@ -17,34 +17,27 @@ const password = process.env.API_PASSWORD;
 const credentials = Buffer.from(`${username}:${password}`).toString("base64");
 
 async function fetchVehicleData() {
-  try {
-    const response = await fetch(apiURL, {
-      headers: {
-        Authorization: `Basic ${credentials}`,
-      },
-    });
+  const response = await fetch(apiURL, {
+    headers: {
+      Authorization: `Basic ${credentials}`,
+    },
+  });
 
-    if (!response.ok) {
-      throw new Error(`API responded with status ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching vehicle data:", error);
-    return null;
+  if (!response.ok) {
+    throw new Error(`API responded with status ${response.status}`);
   }
+
+  return await response.json();
 }
 
 app.get("/api/vehicles", async (req, res) => {
   try {
     const data = await fetchVehicleData();
     res.setHeader("Cache-Control", "s-maxage=120, stale-while-revalidate");
-    res.json(data);
+    res.status(200).json(data);
   } catch (error) {
-    res
-      .status(503)
-      .json({ message: "Service unavailable, please try again later." });
+    console.error("Error fetching vehicle data:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
